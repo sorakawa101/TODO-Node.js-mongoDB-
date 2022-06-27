27d1a21260c22d2403117576eee3f23cb6d9cedb
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2022-06-28 01:53:19 +0900

    add: クライアントサイドからデータを作成
    script.jsを追記

--- public/script.js
+++ public/script.js
@@ -1,1 +1,35 @@
-console.log(axios);
+const tasksDOM = document.querySelector(".tasks");
+
+// /api/v1/tasksからタスクを読み込む
+const showTasks = async () => {
+    try {
+        // 自作のAPIを叩く
+        const { data: tasks } = await axios.get("/api/v1/tasks");
+
+        // タスクを出力
+        const allTasks = tasks.map((task) => {
+            const { completed, _id, name } = task;
+
+            return `<div class="single-task">
+            <h5>
+                <span><i class="fas fa-check-circle"></i></span>${name}
+            </h5>
+            <div class="task-links">
+                <!-- 編集リンク -->
+                <a href="#" class="edit-link">
+                    <i class="fas fa-edit"></i>
+                </a>
+                <!-- ゴミ箱リンク -->
+                <a href="#" class="delete-btn">
+                    <i class="fas fa-trash-alt"></i>
+                </a>
+            </div>
+        </div>`;
+        }).join("");
+        tasksDOM.innerHTML = allTasks;
+    } catch(err) {
+        console.log(err);
+    }
+};
+
+showTasks();
